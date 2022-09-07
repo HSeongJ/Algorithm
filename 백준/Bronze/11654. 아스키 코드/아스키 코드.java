b29f5cadc45b098156bae63a174706658a5751f6
조현성 <58585287+HSeongJ@users.noreply.github.com>
2022-09-07 10:41:49 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		int ch = scan.next().charAt(0);
+		
+		System.out.println(ch);
+		
+	}
+
+}
