ea0b742243eb56adaca45e50403500fa80d5c4d4
조현성 <58585287+HSeongJ@users.noreply.github.com>
2022-09-07 10:36:30 +0900

    [Bronze II] Title: 상수, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringBuffer sb = new StringBuffer(br.readLine());
+		
+		sb.reverse();
+		
+		String[] str = sb.toString().split(" ");
+		
+		if(Integer.parseInt(str[0]) > Integer.parseInt(str[1]))
+			System.out.println(Integer.parseInt(str[0]));
+		else
+			System.out.println(Integer.parseInt(str[1]));
+		
+		br.close();
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
