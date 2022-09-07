0401812f1c56bfd5541d5fafc05f243759091255
조현성 <58585287+HSeongJ@users.noreply.github.com>
2022-09-07 10:35:20 +0900

    [Bronze II] Title: 블랙잭, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		int M = scan.nextInt();
+		
+		int max = 0;
+		
+		int[] card = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			card[i] = scan.nextInt();
+		}
+		
+		for(int i = 0; i < N - 2; i++) {
+			for(int j = i + 1; j < N - 1; j++) {
+				for(int k = j + 1; k < N; k++) {
+					if((card[i] + card[j] + card[k] <= M) && (card[i] + card[j] + card[k] > max)) {
+						max = card[i] + card[j] + card[k];
+					}
+				}
+			}
+		}
+		
+		System.out.println(max);
+	}
+
+}
