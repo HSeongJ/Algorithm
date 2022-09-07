a8414678b765e4785783789623da3ff16f32eb87
조현성 <58585287+HSeongJ@users.noreply.github.com>
2022-09-07 10:41:40 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 124 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            try {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+                int C = Integer.parseInt(st.nextToken());
+
+                int result = Math.max(B - A, C - B);
+                System.out.println(result - 1);
+            } catch(Exception e) {
+                break;
+            }
+        }
+        br.close();
+    }
+}
