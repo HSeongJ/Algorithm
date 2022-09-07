8a504273f1c6dff2b3b829b00e0cc3adbc231834
조현성 <58585287+HSeongJ@users.noreply.github.com>
2022-09-07 10:35:12 +0900

    [Bronze II] Title: 피보나치 수, Time: 128 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int first = 0;
+		int second = 1;
+		
+		for(int i = 1; i < n; i++) {
+			int temp = second;
+			
+			second += first;
+			first = temp;
+		}
+		
+		System.out.println(second);
+		
+		br.close();
+	}
+
+}
