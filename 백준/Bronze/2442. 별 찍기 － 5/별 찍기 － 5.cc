f6fec8d351fde6c6e97f5ba5e29e4879b8a5b078
HSeongJ <58585287+HSeongJ@users.noreply.github.com>
2022-10-11 23:38:05 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, i, j;
+
+    cin >> a;
+
+    for(i = 0; i < a; i++) {
+        for(j = 0; j < a - i - 1; j++) {
+            cout << " ";
+        }
+        for(j = 0; j < 1 + (2*i); j++) {
+            cout << "*";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
