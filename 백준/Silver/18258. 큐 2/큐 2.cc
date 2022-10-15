e15ecf427cdb90db71b8b5fa519193f3040aea90
HSeongJ <58585287+HSeongJ@users.noreply.github.com>
2022-10-15 19:29:15 +0900

    [Silver IV] Title: 큐 2, Time: 388 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    queue<int> q;
+    int N;
+
+    cin >> N;
+
+    while(N--) {
+        string s;
+
+        cin >> s;
+
+        if(s == "push") {
+            int X;
+
+            cin >> X;
+
+            q.push(X);
+        } else if (s == "pop") {
+            if(q.empty()) {
+                cout << -1 << '\n';
+            } else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        } else if(s == "size") {
+            cout << q.size() << '\n';
+        } else if(s == "empty") {
+            if(q.empty()) cout << 1 << '\n';
+            else cout << 0 << '\n';
+        } else if(s == "front") {
+            if(q.empty()) cout << -1 << '\n';
+            else cout << q.front() << '\n';
+        } else if(s == "back") {
+            if(q.empty()) cout << -1 << '\n';
+            else cout << q.back() << '\n';
+        }
+    }
+    return 0;
+}
