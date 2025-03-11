9b1e583375a6a0b48f8547539a89e21a65380993
HSeongJ <58585287+HSeongJ@users.noreply.github.com>
2025-03-11 23:33:24 +0900

    [Bronze II] Title: Strfry, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.cc
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -2,44 +2,47 @@
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    int N, a_ary[26], b_ary[26], check;
+  int N;
+  cin >> N;
 
-    cin >> N;
+  string a, b;
+  while (N--)
+  {
+    bool isPossible = true;
+    vector<int> aCount(26), bCount(26);
+    cin >> a >> b;
 
-    string a, b;
-
-    for (int i = 0; i < N; i++) {
-        check = 0;
-        fill(a_ary, a_ary + 26, 0);
-        fill(b_ary, b_ary + 26, 0);
-
-        cin >> a >> b;
-
-        for (int j = 0; j < a.size(); j++) {
-            a_ary[a.at(j) - 'a']++;
-        }
-
-        for (int j = 0; j < b.size(); j++) {
-            b_ary[b.at(j) - 'a']++;
-        }
-
-        for (int j = 0; j < 26; j++) {
-            if(a_ary[j] != b_ary[j]) {
-                check = 1;
-                break;
-            }
-        }
-
-        if(check == 1) {
-            cout << "Impossible\n";
-        } else {
-            cout << "Possible\n";
-        }
+    for (int i = 0; i < a.length(); i++)
+    {
+      aCount[a[i] - 'a']++;
     }
 
-    return 0;
+    for (int i = 0; i < b.length(); i++)
+    {
+      bCount[b[i] - 'a']++;
+    }
+
+    for (int i = 0; i < aCount.size(); i++)
+    {
+      if (aCount[i] != bCount[i])
+      {
+        isPossible = false;
+        break;
+      }
+    }
+
+    if (isPossible)
+    {
+      cout << "Possible\n";
+    }
+    else
+    {
+      cout << "Impossible\n";
+    }
+  }
 }
