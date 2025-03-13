ac280a19ab503dc23c2864fd8a2e9d44cf8452ca
HSeongJ <58585287+HSeongJ@users.noreply.github.com>
2025-03-13 20:29:47 +0900

    [Silver IV] Title: 덱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.cc
+++ 백준/Silver/10866. 덱/덱.cc
@@ -2,52 +2,83 @@
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    deque<int> d;
-    int N;
-
-    cin >> N;
-
-    while(N--) {
-        string s;
-
-        cin >> s;
-
-        if(s == "push_front") {
-            int X;
-            cin >> X;
-            d.push_front(X);
-        } else if(s == "push_back") {
-            int X;
-            cin >> X;
-            d.push_back(X);
-        } else if(s == "pop_front") {
-            if(d.empty()) cout << -1 << '\n';
-            else {
-                cout << d.front() << '\n';
-                d.pop_front();
-            }
-        } else if(s == "pop_back") {
-            if(d.empty()) cout << -1 << '\n';
-            else {
-                cout << d.back() << '\n';
-                d.pop_back();
-            }
-        } else if(s == "size") {
-            cout << d.size() << '\n';
-        } else if(s == "empty") {
-            if(d.empty()) cout << 1 << '\n';
-            else cout << 0 << '\n';
-        } else if(s == "front") {
-            if(d.empty()) cout << -1 << '\n';
-            else cout << d.front() << '\n';
-        } else if(s == "back") {
-            if(d.empty()) cout << -1 << '\n';
-            else cout << d.back() << '\n';
-        }
+  deque<int> d;
+  int N;
+  cin >> N;
+  while (N--)
+  {
+    string s;
+    cin >> s;
+    if (s == "push_front")
+    {
+      int num;
+      cin >> num;
+      d.push_front(num);
     }
-    return 0;
+    else if (s == "push_back")
+    {
+      int num;
+      cin >> num;
+      d.push_back(num);
+    }
+    else if (s == "pop_front")
+    {
+      if (d.empty())
+      {
+        cout << -1 << '\n';
+      }
+      else
+      {
+        cout << d.front() << '\n';
+        d.pop_front();
+      }
+    }
+    else if (s == "pop_back")
+    {
+      if (d.empty())
+      {
+        cout << -1 << '\n';
+      }
+      else
+      {
+        cout << d.back() << '\n';
+        d.pop_back();
+      }
+    }
+    else if (s == "size")
+    {
+      cout << d.size() << '\n';
+    }
+    else if (s == "empty")
+    {
+      cout << d.empty() << '\n';
+    }
+    else if (s == "front")
+    {
+      if (d.empty())
+      {
+        cout << -1 << '\n';
+      }
+      else
+      {
+        cout << d.front() << '\n';
+      }
+    }
+    else if (s == "back")
+    {
+      if (d.empty())
+      {
+        cout << -1 << '\n';
+      }
+      else
+      {
+        cout << d.back() << '\n';
+      }
+    }
+  }
 }
