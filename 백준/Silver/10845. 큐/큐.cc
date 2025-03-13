dfa559b7e593b13120281e16bffadec533b5444b
HSeongJ <58585287+HSeongJ@users.noreply.github.com>
2025-03-13 20:11:26 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -2,44 +2,74 @@
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    int N;
-    queue<int> q;
+  queue<int> q;
 
-    cin >> N;
+  int N;
+  cin >> N;
+  while (N--)
+  {
+    string s;
+    cin >> s;
 
-    while (N--) {
-        string s;
-
-        cin >> s;
-
-        if (s == "push") {
-            int X;
-            cin >> X;
-
-            q.push(X);
-        } else if (s == "pop") {
-            if(q.empty()) {
-                cout << -1 << '\n';
-            } else {
-                cout << q.front() << '\n';
-                q.pop();
-            }
-        } else if (s == "size") {
-            cout << q.size() << '\n';
-        } else if (s == "empty") {
-            if(q.empty()) cout << 1 << '\n';
-            else cout << 0 << '\n';
-        } else if (s == "front") {
-            if(q.empty()) cout << -1 << '\n';
-            else cout << q.front() << '\n';
-        } else if (s == "back") {
-            if(q.empty()) cout << -1 << '\n';
-            else cout << q.back() << '\n';
-        }
+    if (s == "push")
+    {
+      int num;
+      cin >> num;
+      q.push(num);
     }
-    return 0;
+    else if (s == "pop")
+    {
+      if (q.empty())
+      {
+        cout << -1;
+      }
+      else
+      {
+        cout << q.front();
+        q.pop();
+      }
+
+      cout << '\n';
+    }
+    else if (s == "size")
+    {
+      cout << q.size() << '\n';
+    }
+    else if (s == "empty")
+    {
+      cout << q.empty() << '\n';
+    }
+    else if (s == "front")
+    {
+      if (q.empty())
+      {
+        cout << -1;
+      }
+      else
+      {
+        cout << q.front();
+      }
+
+      cout << '\n';
+    }
+    else
+    {
+      // back
+      if (q.empty())
+      {
+        cout << -1;
+      }
+      else
+      {
+        cout << q.back();
+      }
+
+      cout << '\n';
+    }
+  }
 }
