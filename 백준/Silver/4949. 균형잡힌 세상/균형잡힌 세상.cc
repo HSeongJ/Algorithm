feb7d45640f79c53b48cf193c398833babbb1a28
HSeongJ <58585287+HSeongJ@users.noreply.github.com>
2025-03-13 23:55:53 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -2,40 +2,65 @@
 
 using namespace std;
 
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    while(true) {
-        bool valid = true;
-        string s;
-        stack<char> stack;
+  while (true)
+  {
+    bool isError = false;
+    stack<char> st;
+    string s;
 
-        getline(cin, s);
-        if(s == ".") break;
-        
-        for(int i = 0; i < s.size(); i++) {
+    getline(cin, s);
 
-            if(s[i] == '(' || s[i] == '[') {
-                stack.push(s[i]);
-            } else if(s[i] == ')') {
-                if(stack.empty() || stack.top() == '[') {
-                    valid = false;
-                    break;
-                }
-                stack.pop();
-            } else if(s[i] == ']') {
-                if(stack.empty() || stack.top() == '(') {
-                    valid = false;
-                    break;
-                }
-                stack.pop();
-            }
+    if (s == ".")
+      break;
+
+    for (char c : s)
+    {
+      if (c == '(')
+      {
+        st.push('(');
+      }
+      else if (c == '[')
+      {
+        st.push('[');
+      }
+      else if (c == ')')
+      {
+        if (st.empty() || st.top() != '(')
+        {
+          isError = true;
+          break;
         }
-
-        if(!stack.empty()) valid = false;
-
-        if(valid) cout << "yes\n";
-        else cout << "no\n";
+        else
+        {
+          st.pop();
+        }
+      }
+      else if (c == ']')
+      {
+        if (st.empty() || st.top() != '[')
+        {
+          isError = true;
+          break;
+        }
+        else
+        {
+          st.pop();
+        }
+      }
     }
+
+    if (!st.empty() || isError)
+    {
+      cout << "no\n";
+    }
+    else
+    {
+      cout << "yes\n";
+    }
+  }
 }
