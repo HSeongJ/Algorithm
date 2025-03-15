1c319060011da37a3c9b099969ee096cfe4a0c8a
HSeongJ <58585287+HSeongJ@users.noreply.github.com>
2025-03-15 12:01:00 +0900

    [Gold V] Title: 토마토, Time: 84 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,67 +1,97 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 #define X first
 #define Y second
 
-int board[1001][1001];
-bool vis[1001][1001];
+int board[1002][1002];
+int vis[1002][1002];
+int N, M;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
 
-    int n, m, res = 0;
-    cin >> m >> n;
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= m; j++) {
-            cin >> board[i][j];
-        }
+  cin >> M >> N;
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < M; j++)
+    {
+      cin >> board[i][j];
+    }
+  }
+
+  queue<pair<int, int>> Q;
+
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < M; j++)
+    {
+      if (board[i][j] == 1)
+      {
+        vis[i][j] = 1;
+        Q.push({i, j});
+      }
+    }
+  }
+
+  int lastX, lastY;
+  while (!Q.empty())
+  {
+    pair<int, int> cur = Q.front();
+    Q.pop();
+
+    for (int dir = 0; dir < 4; dir++)
+    {
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+      {
+        continue;
+      }
+      if (board[nx][ny] == -1 || vis[nx][ny] != 0)
+      {
+        continue;
+      }
+
+      vis[nx][ny] = vis[cur.X][cur.Y] + 1;
+      Q.push({nx, ny});
     }
 
-    queue<pair<int,int>> Q;
+    if (Q.empty())
+    {
+      lastX = cur.X;
+      lastY = cur.Y;
+    }
+  }
 
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= m; j++) {
-            if(board[i][j] == 1) {
-                vis[i][j] = 1;
-                Q.push({i, j});
-            }
-        }
+  bool isNotAll = false;
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < M; j++)
+    {
+      if (vis[i][j] == 0 && board[i][j] == 0)
+      {
+        isNotAll = true;
+        break;
+      }
     }
 
-    while(!Q.empty()) {
-        pair<int, int> cur = Q.front();
-        Q.pop();
-
-        for(int dir = 0; dir < 4; dir++) {
-            int nx = cur.X + dx[dir];
-            int ny = cur.Y + dy[dir];
-
-            if(nx < 1 || nx > n || ny < 1 || ny > m) continue;
-            if(vis[nx][ny] || board[nx][ny] == -1) continue;
-
-            vis[nx][ny] = 1;
-            board[nx][ny] = board[cur.X][cur.Y] + 1;
-            Q.push({nx, ny});
-        }
+    if (isNotAll)
+    {
+      break;
     }
+  }
 
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= m; j++) {
-            if(board[i][j] == -1) continue;
-            if(board[i][j] == 0) {
-                cout << -1;
-                return 0;
-            }
-
-            res = max(res, board[i][j]);
-        }
-    }
-
-    cout << res - 1;
-    return 0;
+  if (isNotAll)
+  {
+    cout << -1;
+  }
+  else
+  {
+    cout << vis[lastX][lastY] - 1;
+  }
 }
